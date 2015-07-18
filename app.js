4d9e2073d78ff8a4602f17656591e8712b8f8161
topleft <pete.james.jeffryes@gmail.com>
2015-07-18 12:06:34 -0600

    finished factorial and palindrome

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 
-group = {charlie: 45, Jamal: 62, Sharene: 23, Omar: 5};
+group = {charlie: 45, Jamal: 3, Sharene: 23, Omar: 5};
 
 function oldestPerson(obj) {
   var oldest = 0;
-  var name = "";
+  var name;
   for(key in obj) {
     if(obj[key] > oldest){
       oldest = obj[key];
@@ -14,3 +14,69 @@ function oldestPerson(obj) {
 };
 
 console.log(oldestPerson(group));
+
+function longestWord(str){
+  var word = "";
+  var length = 0;
+  var arr = str.split(" ");
+  for (i in arr){
+    if (i.length > length) {
+      word = i;
+    }
+  }
+  return word;
+}
+
+function longestWordNoPunc(str){
+  var word = "";
+  var l = 0;
+  var arr = str.split(" ");
+  for (var i = 0; i < arr.length; i++){
+    var cleanWord = arr[i].replace(/\W/g, "");
+    console.log(cleanWord);
+    if (cleanWord.length > l) {
+      l = cleanWord.length
+      word = cleanWord;
+    }
+  }
+  return word;
+};
+
+
+function factorial(n) {
+  if (n === 0) {
+    return 1;
+  }
+  else if (n < 1) {
+    return n;
+  }
+  else {
+     n * factorial(n-1)
+  }
+};
+
+
+
+function palindrome(string){
+  // split string into array
+  var stringCopy = string.split("");
+  // if we have check every letter or string length is odd
+  if (stringCopy.length <= 1){
+    return true
+  }
+  // store first and last letter to compare and remove them from array
+  var beg = stringCopy.shift();
+  var end = stringCopy.pop();
+  // compare first and last letter
+  if (beg === end) {
+    // run again, join array back into string
+    return palindrome(stringCopy.join(""));
+  }
+  // if beg and end do not match it is not a palindrome
+  else{
+    return false;
+  }
+};
+
+
+console.log(palindrome("racecar"));
